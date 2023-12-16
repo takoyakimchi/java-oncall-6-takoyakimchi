6bb706891f4cd834353bb44e8aec609abbbb8c36
takoyakimchi <c4nyou@gmail.com>
2023-12-16 13:51:42 +0900

    feat(view): InputView 뼈대 작성
    readLine 로직만 작성하고 검증 로직은 아직 미작성

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,18 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static void inputMonthDayOfWeek() {
+        String input = Console.readLine();
+    }
+
+    public static void inputWeekdayWorkers() {
+        String input = Console.readLine();
+    }
+
+    public static void inputHolidayWorkers() {
+        String input = Console.readLine();
+    }
+}
