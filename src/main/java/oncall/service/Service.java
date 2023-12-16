ed02e64556ab3efb579e676867ce90e149c16318
takoyakimchi <c4nyou@gmail.com>
2023-12-16 16:11:09 +0900

    feat(service): 근무자 배정 로직 구현 - 이틀 연속 근무 미구현

---
+++ src/main/java/oncall/service/Service.java
@@ -0,0 +1,55 @@
+package oncall.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import oncall.domain.Date;
+import oncall.domain.DayOfWeek;
+import oncall.domain.WorkSchedule;
+
+public class Service {
+
+    public static List<WorkSchedule> assignWorkSchedule(int month, DayOfWeek firstDay,
+        List<String> weekdayWorkers, List<String> holidayWorkers) {
+
+        List<WorkSchedule> workSchedules = new ArrayList<>();
+        int weekdayIdx = 0;
+        int holidayIdx = 0;
+        int workerCount = weekdayWorkers.size();
+
+        for (int i = 1; i <= lastDateOfMonth(month); i++) {
+            Date date = new Date(month, i, firstDay);
+            if (date.isHoliday()) {
+                workSchedules.add(new WorkSchedule(date, holidayWorkers.get(holidayIdx)));
+                holidayIdx = nextIdx(holidayIdx, workerCount);
+            }
+            if (!date.isHoliday()) {
+                workSchedules.add(new WorkSchedule(date, weekdayWorkers.get(weekdayIdx)));
+                weekdayIdx = nextIdx(weekdayIdx, workerCount);
+            }
+        }
+
+        return workSchedules;
+    }
+
+    private static int nextIdx(int idx, int count) {
+        int result = idx + 1;
+        if (idx == count) {
+            result = 0;
+        }
+        return result;
+    }
+
+    private static int lastDateOfMonth(int month) {
+        if (month == 2) {
+            return 28;
+        }
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        }
+        if (month == 1 || month == 3 || month == 5 || month == 7
+            || month == 8 || month == 10 || month == 12) {
+            return 31;
+        }
+        return -1;
+    }
+}
