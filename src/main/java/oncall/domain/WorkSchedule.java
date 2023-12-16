cec632590cf520e6b05f060ea91173fbe9463af0
takoyakimchi <c4nyou@gmail.com>
2023-12-16 15:28:49 +0900

    feat(domain): WorkSchedule 추가

---
+++ src/main/java/oncall/domain/WorkSchedule.java
@@ -0,0 +1,11 @@
+package oncall.domain;
+
+public class WorkSchedule {
+    private Date date;
+    private String name;
+
+    public WorkSchedule(Date date, String name) {
+        this.date = date;
+        this.name = name;
+    }
+}
